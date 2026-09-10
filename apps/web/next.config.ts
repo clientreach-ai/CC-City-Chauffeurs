f65e17f1de2918ccb6455a9832440b166f44c304
Sifen Fisaha <sifenfisaha@gmail.com>
2026-09-10 16:10:04 +0300

    chore: initial project setup

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@CC-City-Chauffeurs/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
